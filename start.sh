87f1cc86ae8086f75b07eb25820a291ab5886902
David Sabatie <david.sabatie@notrenet.com>
2021-05-16 23:20:53 +0200

    feat: Use args for socat

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Starting socat"
-socat pty,link=/dev/ttyRFX0,raw,user=root,group=dialout,mode=777,waitslave tcp:192.168.10.194:10001,forever,reuseaddr,keepalive &
+echo ${1}
+socat ${1} ${2} &
 
 echo "Starting node-red"
 npm start --cache /data/.npm -- --userDir /data
